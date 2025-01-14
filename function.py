80e22f5050b92771f48c9a1ff04df650ecef7a0e
Dhruv Arora <63711315+DhruvArora02@users.noreply.github.com>
2025-01-13 20:16:05 -0700

    Update function.py

--- function.py
+++ function.py
@@ -17,22 +17,17 @@ def world_map(Z, names, K_clusters):
         cluster1 = Z[step][0]
         cluster2 = Z[step][1]
 
-        # Create new cluster id as n + step
         new_cluster_id = n + step
 
-        # Merge clusters
         clusters[new_cluster_id] = clusters.pop(cluster1) + clusters.pop(cluster2)
 
-    # Assign cluster labels to countries in the world dataset
     for i, value in enumerate(clusters.values()):
         for val in value:
             world.loc[world['name'] == names[val], 'cluster'] = i
 
-    # Plot the map
     world.plot(column='cluster', legend=True, figsize=(15, 10), missing_kwds={
-        "color": "lightgrey",  # Set the color of countries without clusters
+        "color": "lightgrey",  
         "label": "Other countries"
     })
 
-    # Show the plot
     plt.show()
